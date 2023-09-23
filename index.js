e4d23782d56b2798a731f8d87a3cc132cff9a64a
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-23 16:10:51 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam",
+    streetAddress: 'Broadway',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployees = {...employee}; //make a copy of the employee const
+
+    newEmployees[key] = value; // a mutated value
+    return newEmployees;
+
+}
+//console.log(updateEmployeeWithKeyAndValue())
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+//console.log(destructivelyUpdateEmployeeWithKeyAndValue()) //this will appear as undefined since we havent put any real values
+
+function deleteFromEmployeeByKey(employee, key){
+    let anotherEmployee = {...employee};//create copy non-destructively
+
+    delete anotherEmployee[key]; // method that deletes an item from your copy ie is non-destructive
+
+    return anotherEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]; //deletes an item from your original ie is destructive
+
+    return employee
+}
